3e53a6ee10c7137181bed77c302465a6b059bc5a
Ian V <ianenrico1@gmail.com>
2021-09-23 11:04:41 -0400

    React Hook Form added and buttons updated

--- components/Card.jsx
+++ components/Card.jsx
@@ -8,7 +8,7 @@ const Card = (data, index) => {
         <h3 className="mt-4 text-4xl mx-2">{data.heading}</h3>
         <p className="mt-4 text-lg mx-2">{data.text}</p>
       </div>
-      <button className="my-6 mx-2 px-4 py-2 bg-primary hover:bg-green-500 
+      <button className="transition duration-500 ease-in-out my-6 mx-2 px-4 py-2 bg-primary hover:bg-green-500 
        text-white font-semibold text-base ">
         {data.button}
       </button>
--- components/ContactHome.jsx
+++ components/ContactHome.jsx
@@ -1,54 +1,104 @@
+import { useForm } from "react-hook-form";
+
 import CompanySpacer from "./CompanySpacer";
 
 const ContactHome = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("name"));
+
   return (
-    <form className=" bg-gray-100 pb-8 pt-4">
+    <div className=" bg-gray-100 pb-8 pt-4">
       <div className="max-w-7xl  mx-auto">
         <CompanySpacer />
         <h2 className="text-center text-5xl mt-12">Contact Us</h2>
-        <div className=" text-center mt-8">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className=" text-center mt-8 flex flex-col items-start mx-auto w-4/5 sm:w-3/5"
+        >
           <input
             type="text"
-            name="Name"
+            defaultValue=""
+            {...register("name", { required: true })}
+            name="name"
             placeholder="Your Name"
             id=""
-            required
-            className=" shadow-md my-4 w-3/5  font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
+            className=" shadow-md my-4 w-full  font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
           />
+          {errors.name && (
+            <span className="text-red-600 font-semibold">
+              This field is required
+            </span>
+          )}
+
           <input
             type="text"
-            name="Subject"
+            defaultValue=""
+            {...register("subject", { required: true })}
+            name="subject"
             placeholder="Your Issue"
             id=""
-            required
-            className=" my-4 w-3/5 font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
+            className=" shadow-md my-4 w-full font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
           />
+          {errors.subject && (
+            <span className="text-red-600 font-semibold">
+              This field is required
+            </span>
+          )}
           <input
             type="tel"
-            name="Phone"
+            defaultValue=""
+            {...register("phone")}
+            name="phone"
             id=""
             placeholder="Your Phone Number (optional)"
-            className=" my-4 w-3/5 font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
+            className=" shadow-md my-4 w-full font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
           />
           <input
             type="email"
-            name="Email"
+            defaultValue=""
+            {...register("email", { required: true })}
+            name="email"
             id=""
             placeholder="Your Email"
-            required
-            className=" my-4 w-3/5 font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
+            className=" shadow-md my-4 w-full font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500 rounded-md px-6 py-2"
           />
+          {errors.email && (
+            <span className="text-red-600 font-semibold">
+              This field is required
+            </span>
+          )}
           <textarea
-            name="Message"
+            defaultValue=""
+            {...register("message", { required: true })}
+            name="message"
             id=""
             cols="30"
             rows="10"
             placeholder="Tell Us About Your Issue"
-            className=" my-4 w-3/5 font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500  rounded-md px-6 py-2"
+            className=" shadow-md my-4 w-full font-medium border-2 border-gray-900 focus:ring-2 focus:outline-none focus:border-transparent focus:ring-green-500  rounded-md px-6 py-2"
           ></textarea>
-        </div>
+          {errors.message && (
+            <span className="text-red-600 font-semibold">
+              This field is required
+            </span>
+          )}
+          <button
+            type="submit"
+            className="transition duration-500 ease-in-out my-4 w-4/5 sm:w-1/5 py-3 font-semibold  rounded-md bg-primary text-white hover:bg-green-500 "
+          >
+            {" "}
+            Submit
+          </button>
+        </form>
       </div>
-    </form>
+    </div>
   );
 };
 
